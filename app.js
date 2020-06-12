ed089503001ac2e2797a9b6085dd455d2b56346b
Gustavo <Hukummi@live.com>
2020-06-12 19:39:23 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+const router = express.Router();
+
+router.get('/', function(request,response){
+  response.sendFile(path.join(__dirname+'/index.html'));
+})
+//add the router
+app.use('/', router);
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
