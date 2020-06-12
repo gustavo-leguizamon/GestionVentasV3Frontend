1f1e67fa757d74273aa5ee519abd562f0710b92c
Gustavo <Hukummi@live.com>
2020-06-12 20:54:45 -0300

    add data

---
+++ data/products.js
@@ -0,0 +1,42 @@
+[
+  {
+    "productoId": 1,
+    "codigo": "MOU",
+    "marca": "Noganet",
+    "nombre": "Mouse",
+    "imagen": "https://images.samsung.com/is/image/samsung/uk-led-sf350-ls24f350fhuxen-001-front-black?$PD_GALLERY_L_JPG$",
+    "precio": 10
+  },
+  {
+    "productoId": 2,
+    "codigo": "CAM",
+    "marca": null,
+    "nombre": "Camara",
+    "imagen": "https://images.samsung.com/is/image/samsung/uk-led-sf350-ls24f350fhuxen-001-front-black?$PD_GALLERY_L_JPG$",
+    "precio": 15
+  },
+  {
+    "productoId": 3,
+    "codigo": "TEC",
+    "marca": "Noganet",
+    "nombre": "Teclado",
+    "imagen": "https://images.samsung.com/is/image/samsung/uk-led-sf350-ls24f350fhuxen-001-front-black?$PD_GALLERY_L_JPG$",
+    "precio": 20
+  },
+  {
+    "productoId": 4,
+    "codigo": "MON",
+    "marca": "Samsung",
+    "nombre": "Monitor",
+    "imagen": "https://images.samsung.com/is/image/samsung/uk-led-sf350-ls24f350fhuxen-001-front-black?$PD_GALLERY_L_JPG$",
+    "precio": 38
+  },
+  {
+    "productoId": 5,
+    "codigo": "CPU",
+    "marca": "Gigabit",
+    "nombre": "CPU",
+    "imagen": "https://images.samsung.com/is/image/samsung/uk-led-sf350-ls24f350fhuxen-001-front-black?$PD_GALLERY_L_JPG$",
+    "precio": 250
+  }
+]
